9187eaaf3455164f56bd93f8755822529b21cb2e
erin kirk <116248861+erink037@users.noreply.github.com>
2022-12-01 21:23:50 -0600

    Add files via upload
    View demo here: 
    
    https://replit.com/@erinkirk/StrangeMutedKeychanger#index.html

---
+++ script.js
@@ -0,0 +1,16 @@
+<script>
+
+var browser = navigator.userAgent;
+
+console.log(browser)
+
+if (browser.indexOf("Chrome") > -1) {
+
+     alert("Ah ha, you’re using Chrome!");
+
+}
+
+</script>
+
+
+
